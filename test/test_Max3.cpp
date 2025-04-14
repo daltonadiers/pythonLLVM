57e4796121ca34d9bb37443565679546dc9e369a
OtavioFicagna <oaugusto796@gmail.com>
2025-04-13 23:25:33 -0300

    Alterando import

--- test/test_Max3.cpp
+++ test/test_Max3.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-extern int64_t max3(int64_t, int64_t, int64_t);
+extern "C" int64_t max3(int64_t a, int64_t b, int64_t c);
 
 int main() {
     int64_t a = 10, b = 25, c = 15;
